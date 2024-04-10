fdc236ed63951b82a0cf804104ad44690acff9d4
Saul Amador <155937983+SaulAmador@users.noreply.github.com>
2024-04-10 01:29:14 +0000

    añadi jumbotron, cards, y navbar

---
+++ src/js/component/cards.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+function Cards () {
+    return (
+      <div className="row">
+      <div className="col">
+          <div className="card" style={{width: '18rem'}}>
+              <img src="https://picsum.photos/200/200" className="card-img-top" alt="..."/>  
+              <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+                  <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+          </div>
+      </div>
+      <div className="col">
+          <div className="card" style={{width: '18rem'}}>
+              <img src="https://picsum.photos/200/200" className="card-img-top" alt="..."/>  
+              <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+                  <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+          </div>
+      </div>
+      <div className="col">
+          <div className="card" style={{width: '18rem'}}>
+              <img src="https://picsum.photos/200/200" className="card-img-top" alt="..."/>  
+              <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+                  <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+          </div>
+      </div>
+      <div className="col">
+          <div className="card" style={{width: '18rem'}}>
+              <img src="https://picsum.photos/200/200" className="card-img-top" alt="..."/>  
+              <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+                  <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+          </div>
+      </div>
+  </div>
+       );
+}
+
+export default Cards;
